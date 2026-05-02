35b44ec75f0fabefe8a4ef52afa42923a8847b68
yangjun <526838933@qq.com>
2026-05-02 20:53:09 +0800

    add pkg

---
+++ pkg/three_pathfinding_3d_wasm.js
@@ -0,0 +1,9 @@
+/* @ts-self-types="./three_pathfinding_3d_wasm.d.ts" */
+import * as wasm from "./three_pathfinding_3d_wasm_bg.wasm";
+import { __wbg_set_wasm } from "./three_pathfinding_3d_wasm_bg.js";
+
+__wbg_set_wasm(wasm);
+wasm.__wbindgen_start();
+export {
+    PathfindingWasm
+} from "./three_pathfinding_3d_wasm_bg.js";
